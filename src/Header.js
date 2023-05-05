2009b513cc3bae967b1dd91cecafddc820bf9a42
masifmirza927 <masifmirza927@gmail.com>
2023-05-05 11:20:27 +0500

    local bootstrap

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,23 @@
 
+import MyImg from './images/img.jpg'
+import SVG from './images/undraw.svg'
+
 import Navigation from "./Navigation";
 
 function Header(props) {
 
     return(
         <div>
+            <img className='myimg' src={MyImg} />
+
+
             <Navigation />
+
+<img className='svg' src={SVG}  />
+
+            <div>
+                
+            </div>
         </div>
     );
 }
