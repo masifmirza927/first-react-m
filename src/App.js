d0247650e1d93cd112d6e4f6754a8e7fae936a1f
masifmirza927 <masifmirza927@gmail.com>
2023-05-23 12:02:41 +0500

    listing

--- src/App.js
+++ src/App.js
@@ -5,15 +5,29 @@ import MyFirstComp from "./MyFirstComp"
 
 function App() {
 
-  const [fruits, setFruits] = useState(["apple", "mango", "cherry", "peach"]);
+  const [fruits, setFruits] = useState([]);
+  const [inputText, setInputText] = useState("");
 
+  const addItem = () => {
+    const newArr = [...fruits, inputText];
+    setFruits( newArr);
+  }
+
+  const inputUpdate = (event) => {
+    const txt = event.target.value;
+    setInputText(txt);
+  }
+  
 
   return (
     <div className="App">
+      <input onChange={inputUpdate} type='text' />
+
+      <button onClick={addItem}>Add</button>
       <ul>
         {
-          fruits.map( (item) => {
-            return <li>{item}</li>
+          fruits.map( (item, index) => {
+            return <li key={index}>{item}</li>
           })
         }
       </ul>
