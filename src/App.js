8772f57c41879977b8f52db34a0f64dbfc0fa465
masifmirza927 <masifmirza927@gmail.com>
2023-05-29 12:26:54 +0500

    styling

--- src/App.js
+++ src/App.js
@@ -26,8 +26,8 @@ function App() {
     inputField.current.focus();
   }
 
-// we are deleting item from fruits array
-// params: item -> fruit
+  // we are deleting item from fruits array
+  // params: item -> fruit
   const deleteItem = (index) => {
     const result = fruits.toSpliced(index, 1);
     setFruits(result);
@@ -35,42 +35,58 @@ function App() {
 
   const enterKeyPressed = (event) => {
     console.log(event)
-    if(event.key == "Enter") {
+    if (event.key == "Enter") {
       addItem();
     }
   }
-// clear all in prgress tasks
+  // clear all in prgress tasks
   const clearAll = () => {
     setFruits([]);
   }
 
-// handle checkbox
-const handleCheckbox = (event, item, index) => {
-    if(event.target.checked == true) {
+  // handle checkbox
+  const handleCheckbox = (event, item, index) => {
+    if (event.target.checked == true) {
       const newAr = [...completedTasks, item];
       setCompletedTasks(newAr);
       deleteItem(index);
     }
-}  
+  }
 
   return (
     <div className="App">
+      <div className='container'>
+      
+        <div className='inputBox'>
+          <input className='inputTxt' onChange={inputUpdate} type='text' onKeyUp={enterKeyPressed} value={inputText} ref={inputField} />
+          <button className='btn' onClick={addItem}>Add</button>
+        </div>
+        <h1>In Progress Tasks</h1>
+        <div className="listBox">
+          <ul>
+            {
+              fruits.map((item, index) => {
+                return (<li key={index}> <input className='checked' type='checkbox'  onChange={(event) => { handleCheckbox(event, item, index) }} />  <span>{item}</span>
+                  <button className='delIcon' onClick={() => { deleteItem(index) }} >
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style={{"fill": "rgb(204 42 42)"}}><path d="M6 7H5v13a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V7H6zm10.618-3L15 2H9L7.382 4H3v2h18V4z"></path></svg>
+                  </button>
+                </li>)
+              })
+            }
+          </ul>
+        </div>
 
-      <input onChange={inputUpdate} type='text' onKeyUp={enterKeyPressed} value={inputText} ref={inputField}  />
-      <button onClick={addItem}>Add</button>
-
-      <ul>
-        {
-          fruits.map( (item, index) => {
-            return <li key={index}> <input type='checkbox' onChange={ (event) => {handleCheckbox(event, item, index)} } />  {item} <button onClick={ () => { deleteItem(index) } } >X</button> </li>
-          })
-        }
-      </ul>
-
-      <button onClick={clearAll} >Clear All</button>
+       {
+        (fruits.length > 0) ? <button className='btn clearAllBtn' onClick={clearAll} >Clear All</button> : null
+       }       
+       
 
 
+        
+      </div>  {/* container */}
+     
       <Completed completedTasks={completedTasks} />
+
     </div>
   );
 
--- src/Completed.js
+++ src/Completed.js
@@ -1,20 +1,27 @@
 
 
 function Completed(props) {
-    return(
-        <div id="Completed">
-            <h1>Completed</h1>
-
-            <ul>
-        {
-          props.completedTasks.map( (item, index) => {
-            return <li key={index}>{item} <button >X</button> </li>
-          })
-        }
-      </ul>
-
+  return (
+    <div id="Completed">
+      <div className='container'>
+      <h1>Completed Tasks</h1>
+        <div className="listBox">
+          <ul>
+            {
+              props.completedTasks.map((item, index) => {
+                return (<li key={index}> <input className='checked' checked="true" type='checkbox' />  <span>{item}</span>
+                  <button className='delIcon' >
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style={{ "fill": "rgb(204 42 42)" }}><path d="M6 7H5v13a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V7H6zm10.618-3L15 2H9L7.382 4H3v2h18V4z"></path></svg>
+                  </button>
+                </li>)
+              })
+            }
+          </ul>
         </div>
-    )
+
+      </div>
+    </div>
+  )
 }
 
 export default Completed;
